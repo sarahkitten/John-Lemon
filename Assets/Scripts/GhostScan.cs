01a5816c1221707809d768712a7dbc74daac274e
Sarah Kitten <59421998+sarahkitten@users.noreply.github.com>
2021-04-16 20:53:22 -0700

    Init commit

---
+++ Assets/Scripts/GhostScan.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GhostScan : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 _angles;
+
+    public Transform JohnLemon;
+    public Transform Ghost;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _angles = new Vector3(0.0f, 1.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 distance = JohnLemon.position - Ghost.position;
+
+        if (distance.magnitude < 3)
+        {
+
+            float maxSpeed = 1000.0f;
+            float minSpeed = 100.0f;
+            float alpha = 0.1f; // normalized to [0.0, 1.0] scale        
+            float interpolatedSpeed = (1.0f - alpha) * minSpeed + alpha * maxSpeed;
+            transform.Rotate(_angles * interpolatedSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class Rotator : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 _angles;
+    void Start()
+    {
+        // Reuse rather than creating this every update.        
+        _angles = new Vector3(0.0f, 1.0f, 0.0f);
+    }
+
+    void FixedUpdate()
+    {
+        // Calculate vector from pickup to player.        
+        Vector3 d = player.transform.position - transform.position;
+
+        // Normalize to a direction.        
+        d.Normalize();
+
+        float angle = Mathf.Rad2Deg * Mathf.Acos(Vector3.Dot(Vector3.forward, d));
+
+        Vector3 cross = Vector3.Cross(Vector3.forward, d);
+        if (cross.y < 0.0f)
+        {
+            angle = -angle;
+        }
+
+        _angles.y = angle;
+        transform.eulerAngles = _angles;
+
+    }
+}
